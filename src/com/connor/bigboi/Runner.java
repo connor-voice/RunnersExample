916a9994bf96d59d3652a6a631db5f1cae549cd8
connor-voice <connor.voice.683@gmail.com>
2021-11-09 11:14:02 +0000

    fixed work

--- src/com/connor/bigboi/Runner.java
+++ src/com/connor/bigboi/Runner.java
@@ -4,7 +4,9 @@ public class Runner {
 	public static void main(String[] args) {
 		Person connor = new Person("Connor", "Software Apprentice", 24, 165, 7);
 		Person josh = new Person("Josh", "Stock Broker", 46, 192, 11);
-		Person raheem = new Person("Raheem", "Business Analyst", 26, 186, 10);
+		
+		Person raheem = new Person("Raheem", 186);
+		
 		Person lindsey = new Person("Lindsey", "Singer", 22, 156, 4);
 		Person stirling = new Person("Stirling", "Singer", 22, 150, 5);
 		
